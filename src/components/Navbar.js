3406751e7f5074a151bc90a71155ff6b769249ef
Bavon <carlosbavon46@gmail.com>
2025-08-08 10:00:00 +0300

    Adding .active

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 function Navbar({onLogout}) {
+  const location = useLocation();
+
   return (
     <nav className="navbar">
       <h2>Cereals Store</h2>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/products">Products</Link></li>
-        <li><Link to="/stock">Stock</Link></li>
-        <li><Link to="/sales">Sales</Link></li>
+        <li><Link to="/" className={`nav-link ${location.pathname === "/" ? "active" : ""}`}>Home</Link></li>
+        <li><Link to="/products" className={`nav-link ${location.pathname === "/products" ? "active" : ""}`}>Products</Link></li>
+        <li><Link to="/stock" className={`nav-link ${location.pathname === "/stock" ? "active" : ""}`}>Stock</Link></li>
+        <li><Link to="/sales" className={`nav-link ${location.pathname === "/sales" ? "active" : ""}`}>Sales</Link></li>
         <button onClick={onLogout}>Logout</button>
       </ul>
     </nav>
